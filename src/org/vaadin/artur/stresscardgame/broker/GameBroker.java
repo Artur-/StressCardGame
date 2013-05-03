1f361ec049710a23a540a7c596a1360990170a2f
Artur Signell <artur@vaadin.com>
2013-05-03 15:40:58 +0300

    Fixed locking issue in game start

--- src/org/vaadin/artur/stresscardgame/broker/GameBroker.java
+++ src/org/vaadin/artur/stresscardgame/broker/GameBroker.java
@@ -6,21 +6,26 @@ import java.util.UUID;
 
 import org.vaadin.artur.stresscardgame.engine.StressGameEngine;
 
-
 public class GameBroker {
     private static Map<String, BrokerClient> pendingClients = new HashMap<String, BrokerClient>();
 
-    public synchronized static void register(BrokerClient client) {
+    public synchronized static void register(final BrokerClient client) {
         if (pendingClients.isEmpty()) {
             pendingClients.put(UUID.randomUUID().toString(), client);
         } else {
             String uuid = pendingClients.keySet().iterator().next();
             // TODO Check client is still there
-            BrokerClient otherclient = pendingClients.remove(uuid);
+            final BrokerClient otherclient = pendingClients.remove(uuid);
 
-            StressGameEngine gameEngine = new StressGameEngine();
-            client.gameLaunching(gameEngine);
-            otherclient.gameLaunching(gameEngine);
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    StressGameEngine gameEngine = new StressGameEngine();
+                    client.gameLaunching(gameEngine);
+                    otherclient.gameLaunching(gameEngine);
+                }
+            }).start();
         }
     }
 
